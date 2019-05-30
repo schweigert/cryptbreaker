5b8bd900e31a664280e005ee0add4ef55fe8be92
schweigert <marlon.henry@magrathealabs.com>
2019-05-30 20:28:58 -0300

    World

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
+#include <omp.h>
+#include <mpi.h>
+
 #include <stdio.h>
 #include <crypt.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
+#define FRONT_ID 0
 
+int world_size = 1;
+int world_rank = 0;
+
+
+void front() {
+}
+
+void back() {
+
+}
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    printf("rank %d of %d\n", world_rank, world_size);
+
+    if (MPI_Comm_rank == FRONT_ID) {
+        front();
+    } else {
+        back();
+    }
+
+    MPI_Finalize();
     return 0;
 }
--- makefile
+++ makefile
@@ -2,7 +2,10 @@
 all: clear main
 
 main:
-	gcc main.c -o main -lcrypt
+	mpicc main.c -o main -lcrypt
 
 clear:
 	rm -rf main
+
+run:
+	mpirun --mca btl_tcp_if_include wlp1s0 -n 25 -machinefile world.txt main 2 2
